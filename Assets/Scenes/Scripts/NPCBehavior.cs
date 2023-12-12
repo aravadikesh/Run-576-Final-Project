789aaeb6a04ade9e6c5756dd25745d1998f07eac
aravadikesh <aravadikesh@gmail.com>
2023-12-12 12:35:26 -0500

    all files initial upload

---
+++ Assets/Scenes/Scripts/NPCBehavior.cs
@@ -0,0 +1,122 @@
+// Code written by Arav Adikesh Ramakrishnan
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCBehavior : MonoBehaviour
+{
+    // Public variables for inspector adjustments
+    public NavMeshAgent navMeshAgent;               // Reference to the NavMeshAgent component
+    public float startWaitTime = 4;                 // Initial wait time before starting any action
+    public float timeToRotate = 2;                  // Wait time when the enemy detects the player without visual contact
+    public float speedWalk = 6;                     // Walking speed
+    public float speedRun = 9;                      // Running speed
+    public float viewRadius = 15;                   // Radius of the enemy's field of vision
+    public float viewAngle = 90;                    // Angle of the enemy's field of vision
+    public LayerMask playerMask;                    // Layer mask to detect the player
+    public float meshResolution = 1.0f;             // Number of rays cast per degree for environment scanning
+    public int edgeIterations = 4;                  // Number of iterations to enhance mesh filtering when rays hit obstacles
+    public float edgeDistance = 0.5f;               // Maximum distance for raycasting when an obstacle is hit
+    public float chaseRadius = 10;                  // Radius to trigger faster chasing
+    public float minSpeed = 3;                      // Minimum speed when far from player
+    public float maxSpeed = 9;                      // Maximum speed when close to player
+    public float freezeDuration = 5;               // Duration to freeze NPC after achieving an objective
+    private bool isFrozen = false;                  // Flag to check if NPC is frozen
+    
+    // Variables for player detection and chasing
+    private bool m_playerInRange;                   // If the player is in range of vision, state of chasing
+    private Vector3 m_PlayerPosition;               // Last position of the player when the player is seen by the enemy
+
+    void Start()
+    {
+        // Initialize NavMeshAgent and set initial properties
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.isStopped = false;
+        navMeshAgent.speed = speedWalk;
+    }
+
+    void Update()
+    {
+        // Check if the NPC is not frozen before updating its behavior
+        if (!isFrozen)
+        {
+            // Perform environment scanning and chase player if applicable
+            EnviromentView();
+            ChasePlayer();
+        }
+    }
+
+    // Function to handle chasing the player
+    void ChasePlayer()
+    {
+        // Check if the player is in the chase range
+        if (m_playerInRange)
+        {
+            // Move the NPC with speed based on the distance to the player
+            Move(Mathf.Lerp(minSpeed, maxSpeed, Vector3.Distance(transform.position, m_PlayerPosition) / chaseRadius));
+            navMeshAgent.SetDestination(m_PlayerPosition);
+        }
+    }
+
+    // Function to perform environment scanning to detect the player
+    void EnviromentView()
+    {
+        // Detect all colliders within the view radius and player layer
+        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+
+        // Iterate through all detected colliders
+        for (int i = 0; i < playerInRange.Length; i++)
+        {
+            // Get the player's transform and direction to the player
+            Transform player = playerInRange[i].transform;
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+
+            // Calculate the distance to the player
+            float dstToPlayer = Vector3.Distance(transform.position, player.position);
+
+            // Check if the player is within the chase radius
+            if (dstToPlayer <= chaseRadius)
+            {
+                m_playerInRange = true;
+                m_PlayerPosition = player.transform.position;
+            }
+            else
+            {
+                m_playerInRange = false;
+            }
+        }
+    }
+
+    // Function to freeze the NPC for a specified duration
+    void FreezeNPC()
+    {
+        StartCoroutine(FreezeCoroutine());
+    }
+
+    // Coroutine to handle freezing the NPC
+    IEnumerator FreezeCoroutine()
+    {
+        // Set the frozen flag to true and stop the NPC
+        isFrozen = true;
+        Stop();
+        // Wait for the specified duration
+        yield return new WaitForSeconds(freezeDuration);
+        // Set the frozen flag to false after the duration
+        isFrozen = false;
+    }
+
+    // Function to stop the NPC
+    void Stop()
+    {
+        navMeshAgent.isStopped = true;
+        navMeshAgent.speed = 0;
+    }
+
+    // Function to move the NPC with a specified speed
+    void Move(float speed)
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.speed = speed;
+    }
+}
---
+++ Assets/Scenes/Scripts/TerrainGen.cs
@@ -0,0 +1,256 @@
+// Code written by Arav Adikesh Ramakrishnan
+
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGeneratorV2 : MonoBehaviour
+{
+    Mesh mesh;
+    private int MESH_SCALE = 10;
+    public AnimationCurve heightCurve;
+    private Vector3[] vertices;
+    private int[] triangles;
+    private Color[] colors;
+    [SerializeField] private Gradient gradient;
+    public GameObject[] objects;
+    private float minTerrainheight;
+    private float maxTerrainheight;
+    private float lastNoiseHeight;
+    public int xSize;
+    public int zSize;
+    public float scale; 
+    public int octaves;
+    public float lacunarity;
+    public int seed;
+    private System.Random prng;
+    private Vector2[] octaveOffsets;
+    [SerializeField] private Material material;
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    public GameObject questionStationPrefab;   // Prefab for question station
+    public GameObject monsterNPCPrefab;        // Prefab for chasing NPC
+    public int numberOfQuestionStations = 5;   // Number of question stations to spawn
+    private float minimumDistanceBetweenStations = 10;  // Constraint for question station spacing
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateNewMap();
+
+        // Spawn question stations and monster NPC
+        //SpawnQuestionStations();
+        //SpawnMonsterNPC();
+    }
+
+    public void CreateNewMap()
+    {
+        CreateMeshShape();
+        CreateTriangles();
+        ColorMap();
+        UpdateMesh();
+    }
+
+    private void CreateMeshShape()
+    {
+        // Creates seed
+        Vector2[] octaveOffsets = GetOffsetSeed();
+
+        if (scale <= 0) scale = 0.0001f;
+
+        // Generate noise map
+        float[,] noiseMap = Noise.GenerateNoiseMap(xSize + 1, zSize + 1, new NoiseSettings
+        {
+            normalizeMode = Noise.NormalizeMode.Global,
+            scale = scale,
+            octaves = octaves,
+            persistance = 0.5f,
+            lacunarity = lacunarity,
+            seed = seed,
+            offset = octaveOffsets[0] 
+        }, Vector2.zero);
+
+        // Create vertices
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                // Set height of vertices from noise map, scaling and offsetting the values
+                float noiseHeight = noiseMap[x, z] * MESH_SCALE; 
+                SetMinMaxHeights(noiseHeight);
+                vertices[i] = new Vector3(x, noiseHeight, z);
+                i++;
+            }
+        }
+    }
+    
+    private Vector2[] GetOffsetSeed()
+    {
+        seed = Random.Range(0, 1000);
+        // changes area of map
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for (int o = 0; o < octaves; o++)
+        {
+            float offsetX = prng.Next(-100000, 100000);
+            float offsetY = prng.Next(-100000, 100000);
+            octaveOffsets[o] = new Vector2(offsetX, offsetY);
+        }
+        return octaveOffsets;
+    }
+
+    private void SetMinMaxHeights(float noiseHeight)
+    {
+        if (noiseHeight > maxTerrainheight)
+            maxTerrainheight = noiseHeight;
+        if (noiseHeight < minTerrainheight)
+            minTerrainheight = noiseHeight;
+    }
+
+    private void CreateTriangles()
+    {
+        // Need 6 vertices to create a square (2 triangles)
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+
+        // loop through rows
+        for (int z = 0; z < xSize; z++)
+        {
+            // fill all columns in row
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    private void ColorMap()
+    {
+        colors = new Color[vertices.Length];
+
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                int index = z * (xSize + 1) + x;
+                float height = Mathf.InverseLerp(minTerrainheight, maxTerrainheight, vertices[index].y);
+                colors[index] = gradient.Evaluate(height);
+            }
+        }
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+        gameObject.transform.localScale = new Vector3(MESH_SCALE, MESH_SCALE, MESH_SCALE);
+
+        SpawnObjects();
+        GetComponent<NavMeshSurface>().BuildNavMesh();
+    }
+
+    private void SpawnObjects() 
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            // find actual position of vertices in the game
+            Vector3 worldPt = transform.TransformPoint(mesh.vertices[i]);
+            var noiseHeight = worldPt.y;
+            // Stop generation if height difference between 2 vertices is too steep
+            if(System.Math.Abs(lastNoiseHeight - worldPt.y) < MESH_SCALE)
+            {
+                // min height for object generation
+                if (noiseHeight > 15)
+                {
+                    // Chance to generate
+                    if (Random.Range(1, 2) == 1)
+                    {
+                        GameObject objectToSpawn = objects[Random.Range(0, objects.Length)];
+                        //var spawnAboveTerrainBy = noiseHeight * 2;
+                        Instantiate(objectToSpawn, new Vector3(mesh.vertices[i].x * MESH_SCALE, mesh.vertices[i].y * MESH_SCALE, mesh.vertices[i].z * MESH_SCALE), Quaternion.identity);
+                    }
+                }
+            }
+            lastNoiseHeight = noiseHeight;
+        }
+    }
+
+    // // Spawn question stations
+    // private void SpawnQuestionStations()
+    // {
+    //     List<Vector3> questionStationPositions = new List<Vector3>();
+
+    //     for (int i = 0; i < numberOfQuestionStations; i++)
+    //     {
+    //         Vector3 randomPosition = GetRandomPositionOnMap();
+
+    //         // Check if the new position is too close to existing question stations
+    //         while (IsTooCloseToOtherStations(randomPosition, questionStationPositions))
+    //         {
+    //             randomPosition = GetRandomPositionOnMap();
+    //         }
+
+    //         questionStationPositions.Add(randomPosition);
+
+    //         Instantiate(questionStationPrefab, randomPosition, Quaternion.identity);
+    //     }
+    // }
+
+    // // Note : This might infinitely loop, need to test what minDist will work best 
+    // private bool IsTooCloseToOtherStations(Vector3 position, List<Vector3> existingPositions)
+    // {
+    //     foreach (Vector3 existingPosition in existingPositions)
+    //     {
+    //         float distance = Vector3.Distance(position, existingPosition);
+
+    //         // Adjust the minimum distance as needed
+    //         if (distance < minimumDistanceBetweenStations)
+    //         {
+    //             return true; // Too close, try a new position
+    //         }
+    //     }
+
+    //     return false; // Acceptable distance
+    // }
+
+    // // Spawn monster NPC
+    // private void SpawnMonsterNPC()
+    // {
+    //     Vector3 randomPosition = GetRandomPositionOnMap();
+    //     Instantiate(monsterNPCPrefab, randomPosition, Quaternion.identity);
+    // }
+
+    // // Get a random position within the map bounds
+    // private Vector3 GetRandomPositionOnMap()
+    // {
+    //     float randomX = Random.Range(0, xSize);
+    //     float randomZ = Random.Range(0, zSize);
+    //     //float height = GenerateNoiseHeight((int)randomZ, (int)randomX, octaveOffsets);
+        
+    //     // Ensure Y-coordinate is set to the generated noise height for the terrain
+    //     return new Vector3(randomX, 5, randomZ);
+    // }
+
+}
