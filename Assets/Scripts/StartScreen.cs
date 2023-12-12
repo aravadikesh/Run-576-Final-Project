ca6c8722a09f85bdd1434754e9db3c037bba76ca
Seforius <yusefwilsonx@gmail.com>
2023-12-12 15:56:52 -0500

    Fix menu

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,27 +1,38 @@
 /*AUTHOR: EYAL*/
 
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour
 {
-    public Button Tutorial;
-    public Button Play;
+    public Button Tutorial, Play, Back;
+    [SerializeField] private GameObject tutorialScreen;
+    [SerializeField] private GameObject startScreen;
     // Start is called before the first frame update
     void Start()
     {
         Tutorial.onClick.AddListener(RunTutorial);
         Play.onClick.AddListener(RunPlay);
+        Back.onClick.AddListener(BackButton);
     }
 
     void RunTutorial()
     {
+        tutorialScreen.SetActive(true);
+        startScreen.SetActive(false);
+    }
 
+    public void BackButton()
+    {
+        Debug.Log("Back Button");
+        tutorialScreen.SetActive(false);
+        startScreen.SetActive(true);
     }
 
     void RunPlay()
     {
-        SceneManager.LoadScene("YusefScene", LoadSceneMode.Single);
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
     }
 }
